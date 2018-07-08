cff816b90a139cd6a203274ca45e1eae0f212936
mahmoud <m0597731363@gmail.com>
2018-07-08 13:20:54 +0300

    moved files form components folder to home folder

--- src/client/components/home/index.js
+++ src/client/components/home/index.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 
 import Header from '../header';
-import NewProjects from '../newprojects'
-import FeaturedProjects from '../featuredprojects'
-import MediaCenter from '../mediacenter'
+import NewProjects from './newprojects'
+import FeaturedProjects from './featuredprojects'
+import MediaCenter from './mediacenter'
 const images = ['./assets/1.png', './assets/2.jpg'];
 
 import './index.css';
-import OurApps from '../ourapps';
+import OurApps from './ourapps';
 
 class Home extends Component {
     render(){
