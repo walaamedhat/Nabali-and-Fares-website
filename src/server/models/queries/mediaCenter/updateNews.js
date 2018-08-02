bb8a0a409ddf2da5bce08db460b79a5e3a03265a
WalaaMedhat <nicedree@gmail.com>
2018-08-02 12:59:48 +0300

    fix problem

--- src/server/models/queries/mediaCenter/updateNews.js
+++ src/server/models/queries/mediaCenter/updateNews.js
@@ -1,30 +1,49 @@
 const mediaCenter = require('../../schemas/mediaCenterSchema');
 
 module.exports = (newsData,callback) => {
-  console.log(newsData,'newsData newsData');
   mediaCenter.findByIdAndUpdate(newsData._id, {
     $set: {
-              name: newsData.name,
-              date: Date.now(),
-              type: newsData.type,
-              discription: newsData.discription,
-              video: newsData.video,
-              secondaryImages: newsData.secondaryImages,
-              mainImage: newsData.mainImage,
-            },
+      name: newsData.name,
+      date: Date.now(),
+      type: newsData.type,
+      discription: newsData.discription,
+      video: newsData.video,
+      secondaryImages: newsData.secondaryImages,
+      mainImage: newsData.mainImage,
+    },
   },
   { new: true },
   (error, result) => {
-    console.log(error,'error');
     if (error) return callback(error);
   })
-    .then(result => {
-      console.log(result, 'result');
-      callback(null, result)})
-    .catch(err => {
-      console.log(err,'errrrrrrr');
-      callback(error)});
+  .then(res => callback(null, res))
+  .catch(err => callback(err));
 };
+// module.exports = (newsData,callback) => {
+//   console.log(newsData,'newsData newsData');
+//   mediaCenter.findByIdAndUpdate(newsData._id, {
+//     $set: {
+//               name: newsData.name,
+//               date: Date.now(),
+//               type: newsData.type,
+//               discription: newsData.discription,
+//               video: newsData.video,
+//               secondaryImages: newsData.secondaryImages,
+//               mainImage: newsData.mainImage,
+//             },
+//   },
+//   { new: true },
+//   (error, result) => {
+//     console.log(error,'error');
+//     if (error) return callback(error);
+//   })
+//     .then(result => {
+//       console.log(result, 'result');
+//       callback(null, result)})
+//     .catch(err => {
+//       console.log(err,'errrrrrrr');
+//       callback(err)});
+// };
 
 // module.exports = (newsData,callback) => {
 //   if ((newsData.newsType && newsData.newsTitle && newsData.newsDescription && newsData.video
